28ac581142ac2f49ac46d3769e7810fc13ffb82b
Kevin Kelly <kvinklly@gmail.com>
2021-04-17 17:22:39 -0500

    Adding first test

--- api/db.ts
+++ api/db.ts
@@ -10,10 +10,5 @@ export interface Db {
 }
 
 export const db: Db = {
-  posts: [{
-    id: 1,
-    title: 'Nexus',
-    body: '...',
-    published: false
-  }]
+  posts: []
 }
--- api/server.ts
+++ api/server.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from 'apollo-server'
 import { context } from './context'
 import { schema } from './schema'
 
-const server = new ApolloServer({ schema, context })
+export const server = new ApolloServer({ schema, context })
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
